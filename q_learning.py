9af8deabddf9e871347b53db4d4598f2d1af476b
Alex <Alex.J.Crosby19@gmail.com>
2025-12-02 15:40:52 +0000

    instructions for next steps added in q_learning.py

---
+++ q_learning.py
@@ -0,0 +1,8 @@
+"""
+Next steps:
+- Implement Q-table initialisation
+- Add training loop (episodes x steps)
+- Implement epsilon-greedy policy
+- Implement Q-learning update
+- Print or log average reward every N episodes
+"""
