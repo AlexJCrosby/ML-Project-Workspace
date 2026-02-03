b421d78b0665f92ce6f987a8771e71d5fe95d48e
Alex <Alex.J.Crosby19@gmail.com>
2026-02-03 15:36:35 +0000

    changed playback ep

--- playback.py
+++ playback.py
@@ -453,7 +453,7 @@ def main():
 
     # Choose snapshots to compare
     snap_a = "Q_ep0001.npy"
-    snap_b = "Q_ep0025.npy"
+    snap_b = "Q_ep0500.npy"
 
     Q_a = np.load(snap_a)
     Q_b = np.load(snap_b)
